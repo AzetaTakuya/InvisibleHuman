a421ded83f416213ecb9b4b41d3efb9427b59a93
AzetaTakuya <azeta0611@gmail.com>
2021-07-31 20:44:56 +0900

    [add] humanoid body Tracking

---
+++ Assets/InvisibleHuman/VRM/Scripts/HumanoidTracker.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InvisibleHuman.BodyTracking
+{
+    public class HumanoidTracker : MonoBehaviour
+    {
+        private Animator animator;
+        private Animator origin;
+
+        private void Start()
+        {
+            animator = this.GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            origin = BoneHandler.animator;
+            if (origin == null) return;
+
+            var originalHandler = new HumanPoseHandler(origin.avatar, origin.transform);
+            var targetHandler = new HumanPoseHandler(animator.avatar, animator.transform);
+
+            HumanPose humanPose = new HumanPose();
+            originalHandler.GetHumanPose(ref humanPose);
+            targetHandler.SetHumanPose(ref humanPose);
+
+            animator.rootPosition = origin.rootPosition;
+            animator.rootRotation = origin.rootRotation;
+        }
+    }
+
+}
